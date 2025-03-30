114ecbbfacc24e1976c08fed08670f65265605be
DenDmik <dmikdenis@gmail.com>
2025-03-30 12:45:37 +0000

    second

--- src/bot.mjs
+++ src/bot.mjs
@@ -4,10 +4,18 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on("text", msg => msg.reply.text(msg.text))
 
-bot.setMyCommands([
-    {command: '/start', description: 'Начальное приветствие'},
-    {command: '/info', description: 'Получить информацию о пользователе'},
-    {command: '/game', description: 'Игра угадай цифру'},
-])
+// bot.setMyCommands([
+//     {command: '/start', description: 'Начальное приветствие'},
+//     {command: '/info', description: 'Получить информацию о пользователе'},
+//     {command: '/game', description: 'Игра угадай цифру'},
+// ])
+bot.on('message',async (msg) => {
+    console.log(msg.from.username)
+    const text = msg.text
+  const chatId = msg.chat.id;})
+
+  bot.on('/hello', (msg) => {
+    return bot.sendMessage(msg.from.id, `Hello, ${ msg.from.first_name }!`);
+  });
 
 export default bot

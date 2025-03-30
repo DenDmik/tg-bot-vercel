1c64cdfffd33855f0b0d1495cd0f6f15784a3f7e
DenDmik <dmikdenis@gmail.com>
2025-03-30 18:24:35 +0000

    1111

--- src/bot.mjs
+++ src/bot.mjs
@@ -4,11 +4,6 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on("text", msg => msg.reply.text(msg.text))
 
-// bot.setMyCommands([
-//     {command: '/start', description: 'Начальное приветствие'},
-//     {command: '/info', description: 'Получить информацию о пользователе'},
-//     {command: '/game', description: 'Игра угадай цифру'},
-// ])
 bot.on('message',async (msg) => {
     console.log(msg.from.username)
     const text = msg.text

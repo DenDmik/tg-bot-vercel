16c87bcb767726915e77f3dc3c78dee88834db6c
DenDmik <dmikdenis@gmail.com>
2025-03-30 12:13:42 +0000

    first

--- src/bot.mjs
+++ src/bot.mjs
@@ -4,4 +4,10 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on("text", msg => msg.reply.text(msg.text))
 
+bot.setMyCommands([
+    {command: '/start', description: 'Начальное приветствие'},
+    {command: '/info', description: 'Получить информацию о пользователе'},
+    {command: '/game', description: 'Игра угадай цифру'},
+])
+
 export default bot

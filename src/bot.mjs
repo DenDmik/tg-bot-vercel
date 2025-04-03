96819c1cbbdd2ccdba1abe2be0a8b3fed6f083eb
DenDmik <dmikdenis@gmail.com>
2025-04-03 14:51:41 +0000

    1

--- src/bot.mjs
+++ src/bot.mjs
@@ -7,7 +7,13 @@ bot.on("text", msg => msg.reply.text(msg.text))
 bot.on('message',async (msg) => {
     console.log(msg.from.username)
     const text = msg.text
-  const chatId = msg.chat.id;})
+  const chatId = msg.chat.id;
+  if(text === '/start'){
+    bot.sendSticker(chatId,'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/1.webp') 
+  }
+
+
+})
 
   bot.on('/hello', (msg) => {
     return bot.sendMessage(msg.from.id, `Hello, ${ msg.from.first_name }!`);

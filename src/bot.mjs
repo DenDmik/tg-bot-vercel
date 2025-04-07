14a8c0bb50a208eb0539d5e93a1eb520fd58f539
DenDmik <dmikdenis@gmail.com>
2025-04-07 10:12:45 +0000

    1

--- src/bot.mjs
+++ src/bot.mjs
@@ -2,24 +2,28 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on("text", (msg) =>{ msg.reply.text(msg.chat.id)
-  console.log(msg.text)
-  }
-)
-bot.on(['/start', '/back'], msg => {
-
-  let replyMarkup = bot.keyboard([
-      ['/buttons', '/inlineKeyboard'],
-      ['/start', '/hide']
+// bot.on("text", (msg) =>{ msg.reply.text(msg.chat.id)
+//   console.log(msg.text)
+//   }
+// )
+ const replyMarkup = bot.keyboard([
+      ['/WebApp', '/inlineKeyboard'],
+      ['/start', '/hello']
   ], {resize: true});
 
-  return bot.sendMessage(msg.from.id, 'Keyboard example.', {replyMarkup});
+bot.on(['/start'], msg => {
+  bot.sendSticker(msg.chat.id, 'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/1.webp')
+  return bot.sendMessage(msg.chat.id, 'Keyboard example.', {replyMarkup});
 
 });
-bot.on('/start',msg=>{bot.sendSticker(msg.chat.id, 'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/1.webp')})
+
+// bot.on('/start',msg=>{bot.sendSticker(msg.chat.id, 'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/1.webp')})
 
   bot.on('/hello', (msg) => {
-    return bot.sendMessage(msg.from.id, `Hello, ${ msg.from.first_name }!`);
+    return bot.sendMessage(msg.chat.id, `Hello, ${ msg.from.first_name }!`);
   });
+  bot.on("/WebApp", (msg)=>{
+   msg.reply.url('https://next-deploy-dun.vercel.app/')
+  console.log(msg.chat.url)})
 
 export default bot
